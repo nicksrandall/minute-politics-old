168a55b92d662b48dd97589290d51ff8ecabcb72
Nick Randall <nick.randall@domo.com>
2014-10-14 11:11:32 -0600

    oops, I forgot to prefix the env variables

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -4,11 +4,11 @@
 // =================================
 module.exports = {
   // Server IP
-  ip:       process.env.IP || OPENSHIFT__IP ||
+  ip:       process.env.IP || process.env.OPENSHIFT__IP ||
             undefined,
 
   // Server port
-  port:     process.env.PORT || OPENSHIFT__PORT ||
+  port:     process.env.PORT || process.env.OPENSHIFT__PORT ||
             8080,
 
   // MongoDB connection options
