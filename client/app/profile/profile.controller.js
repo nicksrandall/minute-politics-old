58a8fa84f11dfcff8e8b62c8d97fc10339662df5
Nick Randall <nick.randall@domo.com>
2014-10-15 13:55:42 -0600

    adding resource url for mobile video url :

--- client/app/profile/profile.controller.js
+++ client/app/profile/profile.controller.js
@@ -15,6 +15,7 @@ angular.module('stumpIoApp')
         $scope.posts = posts.map(function (item) {
           item.mp4SD = $sce.trustAsResourceUrl(item.mp4SD);
           item.mp4HD = $sce.trustAsResourceUrl(item.mp4HD);
+          item.mobile = $sce.trustAsResourceUrl(item.mobile);
           item.webmSD = $sce.trustAsResourceUrl(item.webmSD);
           item.webmHD = $sce.trustAsResourceUrl(item.webmHD);
           return item;
