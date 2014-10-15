a4522560077ad1c44ac4e2094f16936429ff8696
Nick Randall <nick.randall@domo.com>
2014-10-15 17:16:35 -0600

    adding back in unit tests

--- client/app/profile/profile.controller.spec.js
+++ client/app/profile/profile.controller.spec.js
@@ -1,21 +1,21 @@
 'use strict';
 
-// describe('Controller: ProfileCtrl', function () {
+describe('Controller: ProfileCtrl', function () {
 
-//   // load the controller's module
-//   beforeEach(module('stumpIoApp'));
+  // load the controller's module
+  beforeEach(module('stumpIoApp'));
 
-//   var ProfileCtrl, scope;
+  var ProfileCtrl, scope;
 
-//   // Initialize the controller and a mock scope
-//   beforeEach(inject(function ($controller, $rootScope) {
-//     scope = $rootScope.$new();
-//     ProfileCtrl = $controller('ProfileCtrl', {
-//       $scope: scope
-//     });
-//   }));
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    ProfileCtrl = $controller('ProfileCtrl', {
+      $scope: scope
+    });
+  }));
 
-//   it('should ...', function () {
-//     expect(1).toEqual(1);
-//   });
-// });
+  it('should ...', function () {
+    expect(1).toEqual(1);
+  });
+});
