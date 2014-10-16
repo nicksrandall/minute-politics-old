8dc13401c9d551b671a6c386cbc0503d7f055980
Nick Randall <nick.randall@domo.com>
2014-10-15 18:32:41 -0600

    fixing profile ctrl

--- client/app/profile/profile.controller.js
+++ client/app/profile/profile.controller.js
@@ -1,38 +1,21 @@
 'use strict';
 
 angular.module('stumpIoApp')
-  .controller('ProfileCtrl', function ($scope, Auth, User, $sce, $http) {
+  .controller('ProfileCtrl', function ($scope, Auth, $sce, $http) {
     $scope.isAdmin = Auth.isAdmin;
-    $scope.user = User.get(); //I'll need to set this based on user selected
+    $scope.user = Auth.getUser();
     $scope.posts = [];
 
-    $scope.$on('$viewContentLoaded', function () {
+    $http.get('/api/posts/user/' + $scope.user._id).success(function(posts) {
+      $scope.posts = posts.map(function (item) {
+        item.mp4SD = $sce.trustAsResourceUrl(item.mp4SD);
+        item.mp4HD = $sce.trustAsResourceUrl(item.mp4HD);
+        item.mobile = $sce.trustAsResourceUrl(item.mobile);
+        item.webmSD = $sce.trustAsResourceUrl(item.webmSD);
+        item.webmHD = $sce.trustAsResourceUrl(item.webmHD);
+        return item;
+      });
       sublime.load();
     });
-
-    function getUserPosts () {
-      $http.get('/api/posts/user/' + $scope.user._id).success(function(posts) {
-        $scope.posts = posts.map(function (item) {
-          item.mp4SD = $sce.trustAsResourceUrl(item.mp4SD);
-          item.mp4HD = $sce.trustAsResourceUrl(item.mp4HD);
-          item.mobile = $sce.trustAsResourceUrl(item.mobile);
-          item.webmSD = $sce.trustAsResourceUrl(item.webmSD);
-          item.webmHD = $sce.trustAsResourceUrl(item.webmHD);
-          return item;
-        });
-      });
-    }
-
-    if($scope.user.hasOwnProperty('$promise')) {
-      $scope.user.$promise.then(function() {
-        getUserPosts();
-      }).catch(function() {
-        return false;
-      });
-    } else if($scope.user.hasOwnProperty('role')) {
-      getUserPosts();
-    } else {
-      return false;
-    }
 
   });
