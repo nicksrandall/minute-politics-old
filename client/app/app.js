261605beb62a2c5898dd89200c962bf06d672494
Nick Randall <nick.randall@domo.com>
2014-10-16 20:45:50 -0600

    adding user to root scope

--- client/app/app.js
+++ client/app/app.js
@@ -49,10 +49,10 @@ angular.module('stumpIoApp', [
     // Redirect to login if route requires auth and you're not logged in
     $rootScope.$on('$stateChangeStart', function (event, next) {
       Auth.isLoggedInAsync(function(loggedIn) {
+        $rootScope.currentUser = Auth.getCurrentUser();
         if (next.authenticate && !loggedIn) {
           $location.path('/login');
         }
-        $rootScope.currentUser = Auth.getCurrentUser();
       });
     });
   });
