de604a45a2228c3993125983d2598d69069ea72e
Nick Randall <nick.randall@domo.com>
2014-10-16 22:07:40 -0600

    forgot to add this

--- server/api/post/post.controller.js
+++ server/api/post/post.controller.js
@@ -23,7 +23,8 @@ exports.followed_by = function(req, res) {
   User.findById(req.params.id, function (err, user) {
     if(err) { return handleError(res, err); }
     Post.find()
-      .where('author').in( _.union(user.following, [req.user._id]) )
+      .where('author').in( user.following )
+      .populate('author', 'name party picture office area email')
       .sort('-date')
       .exec(function (err, posts) {
         if (err) return res.status(400).send('Error firding posts.');
